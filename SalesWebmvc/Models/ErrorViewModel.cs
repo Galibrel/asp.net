88de887d2ccc968e49fd084cbefe3e9f96728d5d
João Coimbra <coimbrajoao235@gmail.com>
2023-07-06 20:28:14 -0300

    Projeto criado

---
+++ SalesWebmvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalesWebmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
